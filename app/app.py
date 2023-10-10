036e1ba631d29e806baf89337dcc58720a510a83
hasemizu0507 <hasemizu0507@ezweb.ne.jp>
2023-10-10 17:29:53 +0900

    Bootstrapを利用してトップページを作る

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-# 簡単なindexページの作成を行っています。
+"""
+初期化処理
+"""
 from flask import Flask
 
+# Flaskのインスタンスを生成
 app = Flask(__name__)
+
+"""
+トップページのルーティング
+"""
+from flask import render_template
 
 @app.route("/")
 def index():
-    return "index page"
+    # index.htmlをレンダリングして返す
+    return render_template('index.html')
